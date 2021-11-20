70ebb73b7275825bf0f2078dbb238753232d0885
Leonard94 <vladisla.koleda@yandex.ru>
2021-11-20 22:35:50 +0300

    Сделал нормальную вёрстку

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ const controllers = document.getElementById('controllers')
 const play = document.getElementById('play')
 const progress = document.getElementById('progressBody')
 const progressBar = document.getElementById('progressBar')
-const progressTime = document.getElementById('progressTime')
+const timeProgress = document.getElementById('progressTime')
+const timeDuration = document.getElementById('durationTime')
 
-// video.currentTime - время на видео
+// video.currentTime - текущее время на видео
 // video.duration - продолжительность видео
-// video.playbackRate - продолжительность видео
+// video.playbackRate - скорость видео
 // video.play() - запустить видео
 // video.pause() - остановить видео
 // video.paused - видео на паузе (true\false)
@@ -15,14 +16,12 @@ const progressTime = document.getElementById('progressTime')
 
 
 controllers.addEventListener('click', (e) => {
-    // Pressed btn speed
-    if (e.target.classList.contains('controller__speed')) {
-        video.playbackRate = e.target.getAttribute('data-speed')
-    }
 
-    // Pressed btn rewind
+    // Pressed speed
+    if (e.target.classList.contains('speed')) video.playbackRate = e.target.getAttribute('data-speed')
+
+    // Pressed rewind
     if (e.target.classList.contains('rewind')) video.currentTime += Number(e.target.getAttribute('data-rewind'))
-
 })
 
 
@@ -35,8 +34,8 @@ const togglePlay = () => {
 
 // Смена иконки после нажатия на toggle
 const updateIcon = () => {
-    let icon = video.paused ? 'Play' : 'Pause'
-    play.textContent = icon
+    let icon = video.paused ? 'play' : 'pause'
+    play.src = `/img/icon__${icon}.svg`
 }
 
 
@@ -53,39 +52,44 @@ const updateProgress = () => {
     let currentProgress = (video.currentTime / video.duration) * 100
     progressBar.style.width = `${currentProgress}%`
 
-    // update current time and total time
-    progressTime.textContent = `${updateTime(video.currentTime)} / ${updateTime(video.duration)}`
+    // update current time and total(duration) time
+    progressTime.textContent = `${updateTime(video.currentTime)}`
 }
 
 // При нажатии на progressBar
 progress.addEventListener('click', (e) => {
     const targetProgress = (e.offsetX / progress.offsetWidth) * 100
     progressBar.style.width = `${targetProgress}%`
-    video.currentTime = targetProgress  * video.duration / 100
+    video.currentTime = targetProgress * video.duration / 100
 })
 
-play.addEventListener('click', togglePlay)
 
+play.addEventListener('click', togglePlay)
 video.addEventListener('click', togglePlay)
 video.addEventListener('pause', updateIcon)
 video.addEventListener('play', updateIcon)
 video.addEventListener('timeupdate', updateProgress)
+video.addEventListener('loadedmetadata', () => timeDuration.textContent = ` ${updateTime(video.duration)}`)
 
 
 // JavaScript
-
-// todo UpdateTime (Video Duration) обновляется каждый раз наравне с текущим временем
-// todo обновлять продолжительность видео после загрузки всех метаданных
-// todo можно ли с помощью JS сделать обложку видео на основе любого или определенного кадра? После получения метаданных
+// todo при выборе скорости воспроизведения изменять класс на speed-active у drop-down. И изменять current speed
+// todo сделать возможность изменять громкость звука
+// todo перемотка при нажатии на стрелку клавиатуры
 // todo добавить возможность открывать на весь экран?
+// todo можно ли с помощью JS сделать обложку видео на основе любого или определенного кадра? После получения метаданных
 
 
 // CSS
-// Сделать нормальную вёрстку
-// При наведении на иконку звука должен появляться range
+// todo Сделать адаптив?
+
+// Completed JS
+// UpdateTime (Video Duration) обновляется каждый раз наравне с текущим временем. Исправить
+// Обновлять продолжительность видео после загрузки всех метаданных
+// Прикрутить функционал ускорения видео
+
+// Completed CSS
 // При наведении на скорость воспроизведения, предоставить выбор скорости видео
 // Добавить нормальные иконки для всех кнопок
-// Сделать адаптив
-
-// Completed
-// прикрутить функционал ускорения видео
+// Сделать нормальную вёрстку
+// При наведении на иконку звука должен появляться range
